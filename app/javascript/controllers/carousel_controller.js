b6602d80b98f919b586c0b013d07cc3c8eb030e5
Gabriel Aleixo <gabriel.aleixo061@gmail.com>
2022-12-27 16:03:27 -0300

    added stimulus controler and carousel css and html

---
+++ app/javascript/controllers/carousel_controller.js
@@ -0,0 +1,10 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="carousel"
+export default class extends Controller {
+  connect() {
+    console.log("connected")
+  }
+
+
+}
