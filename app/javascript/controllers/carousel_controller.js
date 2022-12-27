45f8709f06d0e9c277729f0757f16e93a223be50
Gabriel Aleixo <gabriel.aleixo061@gmail.com>
2022-12-27 20:15:24 -0300

    done responsive carousel with stimulus

--- app/javascript/controllers/carousel_controller.js
+++ app/javascript/controllers/carousel_controller.js
@@ -2,9 +2,28 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="carousel"
 export default class extends Controller {
+  static targets = ["gallery", "button"]
+   static values = {
+    interval: { type: Number, default: 0 },
+
+  }
   connect() {
-    console.log("connected")
   }
 
-
+  nextSlide() {
+    this.galleryTargets[this.intervalValue].classList.add("d-none")
+    const isLeft = event.currentTarget.classList.contains("arrow-left")
+    if (isLeft) {
+      this.intervalValue -= 1
+    } else {
+      this.intervalValue += 1
+    }
+    if (this.intervalValue >= this.galleryTargets.length) {
+      this.intervalValue = 0
+    }
+    if (this.intervalValue < 0){
+      this.intervalValue = this.galleryTargets.length - 1
+    }
+    this.galleryTargets[this.intervalValue].classList.remove("d-none")
+  }
 }
